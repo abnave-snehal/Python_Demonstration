1ff53866c49921911ba450635f7aaa5c6807beba
snehal <abnavesnehal7@gmail.com>
2026-02-01 19:54:32 +0530

    file created automatically using file functions

---
+++ Automation/DirectoryAutomationEmptyDeleteReportLogTimeStampSchedule.py
@@ -0,0 +1,82 @@
+import sys
+import os
+import time
+import schedule
+
+def directoryScanner(DirName="Marvellous"):  # Default Argument
+    Border="-"*50
+    timestamp=time.ctime()
+
+    LogFileName="Marvellous%s.log" %(timestamp)
+    LogFileName=LogFileName.replace(" ","_")
+    LogFileName=LogFileName.replace(":","_")
+    
+
+    fobj=open(LogFileName,"w")
+
+    fobj.write(Border+"\n")
+    fobj.write("This is a log file created by Marvellous Automation\n")
+    fobj.write("This is a directory cleaner script\n")
+    fobj.write(Border+"\n")
+
+    
+    ret=False
+
+    ret=os.path.exists(DirName)
+    if(ret == False):
+        print("There is no such directory.")
+        return
+    
+    
+    ret=os.path.isdir(DirName)
+    if(ret == False):
+        print("It is not a directory.")
+        return
+    
+    fileCount=0
+    emptyFileCount=0
+
+    for folderName,subFolder,fileName in os.walk(DirName):
+       
+
+        for Fname in fileName:
+            fileCount=fileCount+1
+
+            Fname=os.path.join(folderName,Fname)
+
+            if(os.path.getsize(Fname) == 0):   # Empty file
+                emptyFileCount=emptyFileCount+1
+                os.remove(Fname)
+
+    
+    fobj.write("Total Files Scanned : "+str(fileCount)+"\n")
+    fobj.write("Totla Empty Files Found : "+str(emptyFileCount)+"\n")
+    fobj.write("This log file is created at : "+timestamp+"\n")
+    fobj.write(Border+"\n")
+    
+    fobj.close()
+
+def main():
+    Border="-"*50
+    print(Border)
+    print("--------- Marvellous Directory Automation --------")
+    print(Border)
+
+    if(len(sys.argv) !=2):
+        print("Invalid number of arguments:")
+        print("Please sepcifiy the name of directory :")
+        return
+
+    # directoryScanner(sys.argv[1])
+    schedule.every(1).minute.do(directoryScanner)
+
+    while(True):
+        schedule.run_pending()
+        time.sleep(1)
+
+    print(Border)
+    print("--------- Marvellous Directory Automation --------")
+    print(Border)
+
+if __name__ == "__main__":
+    main()
